1c2e57d047c67c914ec9f3afa382308a3433bdd8
Victor Wesley <vicwes8737@gmail.com>
2022-02-10 18:51:28 -0300

    navegação dos botões da seção projetos

--- js/function.js
+++ js/function.js
@@ -1,7 +1,36 @@
-function slideProjetos(){
-    var ps = document.querySelector('#ps');
-    var lp = document.querySelector('#lp');
-    var si = document.querySelector('#si');
+function slideProjetos(valor){
+    var botoes = {
+        ps: document.querySelector('#ps'),
+        lp: document.querySelector('#lp'),
+        si: document.querySelector('#si'),
+    }
+
+    var botao = valor;
+
+    switch (botao) {
+        case 1:
+            botoes.ps.style.backgroundColor = '#2e2085';
+            botoes.ps.style.boxShadow = '4px 5px 16px 0px #0000008a'
+            botoes.lp.style.backgroundColor = '#6f6a8f';
+            botoes.si.style.backgroundColor = '#6f6a8f';
+            break;
+        case 2:
+            botoes.ps.style.backgroundColor = '#6f6a8f';
+            botoes.lp.style.backgroundColor = '#2e2085';
+            botoes.lp.style.boxShadow = '4px 5px 16px 0px #0000008a'
+            botoes.si.style.backgroundColor = '#6f6a8f';
+            break;
+
+        case 3: 
+            botoes.ps.style.backgroundColor = '#6f6a8f';
+            botoes.lp.style.backgroundColor = '#6f6a8f';
+            botoes.si.style.backgroundColor = '#2e2085';
+            botoes.si.style.boxShadow = '4px 5px 16px 0px #0000008a'
+            break;
+
+        default:
+            break;
+    }
 
     
 }
